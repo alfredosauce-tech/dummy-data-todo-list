c9972072fa34441caa6a3b4e671a207b59f519d6
alfredo aguado <alfredo.aguadoo14@gmail.com>
2021-04-13 20:00:55 -0500

    pushing up

---
+++ app.js
@@ -0,0 +1,29 @@
+let arrayOfTodos = [
+  {
+  "userId": 14,
+  "id": 1,
+  "title": "delectus aut autem",
+  "completed": false
+},
+{
+  "userId": 20,
+  "id": 2,
+  "title": "delectus aut autem",
+  "completed": false
+}]
+
+const fetchTodos = () => {
+  fetch('https://jsonplaceholder.typicode.com/todos')
+  .then( (response) => response.json())
+  .then( (json) => arrayOfTodos = json)
+}
+
+const logTodos = () => {
+  console.log(arrayOfTodos)
+}
+
+const populateTodos = () => {
+for (let index = 0; index < array0ftodos,length; index++) {
+  const element
+}
+}
